401fed6eddc7e056ec76be9eccc3f1b5ec90224d
Dibbo <DeflatedPickle@gmail.com>
2018-01-03 12:00:43 +0000

    Added a compiler class

---
+++ BastCompiler.py
@@ -0,0 +1,21 @@
+import antlr4
+
+from BastLexer import BastLexer
+from BastListener import BastListener
+from BastParser import BastParser
+
+
+class BastCompiler(BastListener):
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    lexer = BastLexer(antlr4.FileStream("example.bsst"))
+    stream = antlr4.CommonTokenStream(lexer)
+    parser = BastParser(stream)
+    tree = parser.program()
+
+    interpret = BastCompiler()
+    walker = antlr4.ParseTreeWalker()
+    walker.walk(interpret, tree)
